b22b9652e2e24dfeac0e690438aa24d914469b27
Семен В. Сергеев <svsergeev@yoomoney.ru>
2025-02-28 16:00:34 +0300

    Fixed nameRepairStrategy for ScheduleParser to more loyal parsing empty column names

--- src/main/kotlin/excel/parser/ScheduleParser.kt
+++ src/main/kotlin/excel/parser/ScheduleParser.kt
@@ -2,13 +2,15 @@ package org.example.excel.parser
 
 import org.example.excel.utils.ScheduleFile
 import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.io.NameRepairStrategy
 import org.jetbrains.kotlinx.dataframe.io.readExcel
 
 class ScheduleParser : ExcelParser<ScheduleFile> {
     override fun parse(file: ScheduleFile): DataFrame<*> {
         return DataFrame.readExcel(
-            file.getScheduleFile(),
-            sheetName = "Sheet1"
+            file = file.getScheduleFile(),
+            sheetName = "Sheet1",
+            nameRepairStrategy = NameRepairStrategy.DO_NOTHING
         ).also {
             file.deleteTempFile()
         }
